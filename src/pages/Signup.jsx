bc5c556c3fcfefce8a55b74994735f7df6a08766
AmeyTari2002 <ameytari2002@gmail.com>
2024-02-25 00:45:40 +0530

    first commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <div className='mx-7 mb-2 md:mx-7 h-[171px] flex flex-col md:flex-row      md:flex items-center justify-between'>
+        <div className='mx-11    mb-2 md:mx-7 h-[171px] flex flex-col  md:flex-row      md:flex items-center md:justify-between'>
             <div className='h-[88px] md:w-[398px] justify-center text-[20px] md:text-[32px] text-[#5C6874] leading-[44px]'>Sign up and get exclusive special deals</div>
             <div className='flex pl-40'>
                 <div className='md:w-[348px] md:h-[52px] bg-white rounded-tl-[12px] rounded-bl-[12px]'></div>
